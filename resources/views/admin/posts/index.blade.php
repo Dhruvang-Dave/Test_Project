13bbe03a740cf58279f1358d22096b7f9a9efdac
Dhruvang <d710529@gmail.com>
2022-07-02 18:27:46 +0530

    Ready to end

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,28 +4,28 @@
 
 <x-settings heading="Publish New Post">
 <form action="/admin/posts" method="POST" class="w-75 border border-gray-200 p-8 rounded-xl m-10" enctype="multipart/form-data">    
-Starting
 
 <table class="table table-striped table-hover">
-<thead>
+<thead> <span class="bg-yellow-200 p-2 rounded-xl"> All Posts</span>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">The Post Title</th>
-      <th scope="col">Published</th>
-      <th scope="col">Edit</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
     @foreach ( $okay as $ok )
         <tr>
           <th scope="row">1</th>
-          <td></td>
-          <td>Otto</td>
-          <td><a href=""></a>Edit</td>
+          <td>{!! $ok->title !!}</td>
+          <td>Published</td>
+          <td><a href="/admin/posts/{{ $ok->id}}/edit">Edit</a></td>
         </tr>
     @endforeach
   </tbody>
 </table>
+{!! $okay->links() !!}
 </form>
 </x-settings>
     
